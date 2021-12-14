0efa9648e1b65c05eaa06d4d76825d3d139d8a5e
Dixieland-Money <96084322+DDepot@users.noreply.github.com>
2021-12-13 22:11:30 -0700

    Update blockchain.ts

--- src/constants/blockchain.ts
+++ src/constants/blockchain.ts
@@ -1,7 +1,7 @@
 export const TOKEN_DECIMALS = 18;
 
 export enum Networks {
-    SGB = ??????,                       //fix
+    SGB = 19,                       //not sure if chian id is correct
 }
 
 export const DEFAULT_NETWORK = Networks.SGB;
