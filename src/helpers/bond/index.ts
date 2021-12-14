e4446dfb122515ca45d513048e1384b6426e959d
Dixieland-Money <96084322+DDepot@users.noreply.github.com>
2021-12-14 13:28:35 -0700

    Update index.ts

--- src/helpers/bond/index.ts
+++ src/helpers/bond/index.ts
@@ -2,75 +2,75 @@ import { Networks } from "../../constants/blockchain";
 import { LPBond, CustomLPBond } from "./lp-bond";
 import { StableBond, CustomBond } from "./stable-bond";
 
-import MimIcon from "../../assets/tokens/MIM.svg";
-import AvaxIcon from "../../assets/tokens/AVAX.svg";
-import MimTimeIcon from "../../assets/tokens/TIME-MIM.svg";
-import AvaxTimeIcon from "../../assets/tokens/TIME-AVAX.svg";
+import GuacIcon from "../../assets/tokens/GUAC.svg";
+import SgbIcon from "../../assets/tokens/SGB.svg";
+import GuacJazzIcon from "../../assets/tokens/JAZZ-GUAC.svg";
+import SgbJazzIcon from "../../assets/tokens/JAZZ-SGB.svg";
 
-import { StableBondContract, LpBondContract, WavaxBondContract, StableReserveContract, LpReserveContract } from "../../abi";
+import { StableBondContract, LpBondContract, WsgbBondContract, StableReserveContract, LpReserveContract } from "../../abi";
 
-export const mim = new StableBond({
-    name: "mim",
-    displayName: "MIM",
-    bondToken: "MIM",
-    bondIconSvg: MimIcon,
+export const guac = new StableBond({
+    name: "guac",
+    displayName: "GUAC",
+    bondToken: "GUAC",
+    bondIconSvg: GuacIcon,
     bondContractABI: StableBondContract,
     reserveContractAbi: StableReserveContract,
     networkAddrs: {
-        [Networks.AVAX]: {
-            bondAddress: "0x694738E0A438d90487b4a549b201142c1a97B556",
-            reserveAddress: "0x130966628846BFd36ff31a822705796e8cb8C18D",
+        [Networks.SGB]: {
+            bondAddress: "0x694738EXXXXXXXXXXXXXXXXXXXXXX7B556",
+            reserveAddress: "0x130966628XXXXXXXXXXXXXXXXXXXC18D",
         },
     },
-    tokensInStrategy: "60500000000000000000000000",
+    tokensInStrategy: "60500000000000000000000000",                                  //edit?
 });
 
-export const wavax = new CustomBond({
-    name: "wavax",
-    displayName: "wAVAX",
-    bondToken: "AVAX",
-    bondIconSvg: AvaxIcon,
-    bondContractABI: WavaxBondContract,
+export const wsgb = new CustomBond({
+    name: "wsgb",
+    displayName: "wSGB",
+    bondToken: "SGB",
+    bondIconSvg: SgbIcon,
+    bondContractABI: WsgbBondContract,
     reserveContractAbi: StableReserveContract,
     networkAddrs: {
-        [Networks.AVAX]: {
-            bondAddress: "0xE02B1AA2c4BE73093BE79d763fdFFC0E3cf67318",
-            reserveAddress: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7",
+        [Networks.SGB]: {
+            bondAddress: "0xE02B1AA2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX7318",
+            reserveAddress: "0xb31f66aa3c1eXXXXXXXXXXXXXXXXXXb85fd66c7",
         },
     },
-    tokensInStrategy: "756916000000000000000000",
+    tokensInStrategy: "756916000000000000000000",                               //edit?
 });
 
-export const mimTime = new LPBond({
-    name: "mim_time_lp",
-    displayName: "TIME-MIM LP",
-    bondToken: "MIM",
-    bondIconSvg: MimTimeIcon,
+export const guacJazz = new LPBond({
+    name: "guac_jazz_lp",
+    displayName: "JAZZ-GUAC LP",
+    bondToken: "GUAC",
+    bondIconSvg: GuacJazzIcon,
     bondContractABI: LpBondContract,
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
-        [Networks.AVAX]: {
-            bondAddress: "0xA184AE1A71EcAD20E822cB965b99c287590c4FFe",
-            reserveAddress: "0x113f413371fc4cc4c9d6416cf1de9dfd7bf747df",
+        [Networks.SGB]: {
+            bondAddress: "0xA184AE1A71XXXXXXXXXXXXXXXXXXXXXXXXXXXXc4FFe",
+            reserveAddress: "0x113f4133XXXXXXXXXXXXXXXXXXXXXXd7bf747df",
         },
     },
-    lpUrl: "https://www.traderjoexyz.com/#/pool/0x130966628846BFd36ff31a822705796e8cb8C18D/0xb54f16fB19478766A268F172C9480f8da1a7c9C3",
+    lpUrl: "https://www.traderjoexyz.com/#/pool/0x130966628846BFd36ff31a822705796e8cb8C18D/0xb54f16fB19478766A268F172C9480f8da1a7c9C3",    //edit to dex?
 });
 
-export const avaxTime = new CustomLPBond({
-    name: "avax_time_lp",
-    displayName: "TIME-AVAX LP",
-    bondToken: "AVAX",
-    bondIconSvg: AvaxTimeIcon,
+export const sgbJazz = new CustomLPBond({
+    name: "sgb_jazz_lp",
+    displayName: "JAZZ-SGB LP",
+    bondToken: "SGB",
+    bondIconSvg: SgbJazzIcon,
     bondContractABI: LpBondContract,
     reserveContractAbi: LpReserveContract,
     networkAddrs: {
-        [Networks.AVAX]: {
-            bondAddress: "0xc26850686ce755FFb8690EA156E5A6cf03DcBDE1",
-            reserveAddress: "0xf64e1c5B6E17031f5504481Ac8145F4c3eab4917",
+        [Networks.SGB]: {
+            bondAddress: "0xc26850686ce7XXXXXXXXXXXXXXXXXXXXXXXXDcBDE1",
+            reserveAddress: "0xf64e1c5BXXXXXXXXXXXXXXXXXXXXXXc3eab4917",
         },
     },
-    lpUrl: "https://www.traderjoexyz.com/#/pool/AVAX/0xb54f16fB19478766A268F172C9480f8da1a7c9C3",
+    lpUrl: "https://www.traderjoexyz.com/#/pool/AVAX/0xb54f16fB19478766A268F172C9480f8da1a7c9C3",                  //edit to dex?
 });
 
-export default [mim, wavax, mimTime, avaxTime];
+export default [guac, wsgb, guacJazz, sgbJazz];
