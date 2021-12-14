8fbc0babe146501602dce34e25e1db3a477f9645
Dixieland-Money <96084322+DDepot@users.noreply.github.com>
2021-12-13 21:48:19 -0700

    Update index.tsx

--- src/views/Landing/components/Main/index.tsx
+++ src/views/Landing/components/Main/index.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { Link } from "@material-ui/core";
 import "./main.scss";
-import CatImg from "../../../../assets/icons/Chershire_Cat.jpeg";
+import JazzImg from "../../../../assets/icons/XXXXXXXXXXLOGOXXXXXXXXXXXXXXXXXXXXXXX.jpeg";      //need Jazz Img
 
 function Main() {
     return (
         <div className="landing-main">
             <div className="landing-main-img-wrap">
-                <img src={CatImg} alt="" />
+                <img src={JazzImg} alt="" />
             </div>
             <div className="landing-main-btns-wrap">
-                <Link href="https://app.wonderland.money" target="_blank" rel="noreferrer">
+                <Link href="https://app.dixieland.money" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
                         <p>Enter App</p>
                     </div>
                 </Link>
-                <Link href="https://wonderland.gitbook.io/wonderland/" target="_blank" rel="noreferrer">
+                <Link href="https://dixieland.gitbook.io/dixieland/" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
                         <p>Documentation</p>
                     </div>
                 </Link>
             </div>
             <div className="landing-main-title-wrap">
-                <p>The Defi</p>
-                <p>WonderLand</p>
+                <p>Coming Soon</p>
+                <p>DixieLand</p>
             </div>
             <div className="landing-main-help-text-wrap">
-                <p>Financial tools to grow your wealth - stake</p>
-                <p>and earn compounding interest</p>
+                <p>a Decentralized reserve currency protocol built on the Songbird Network. Stake</p>
+                <p>and earn compounding interest with JAZZ</p>
             </div>
         </div>
     );
