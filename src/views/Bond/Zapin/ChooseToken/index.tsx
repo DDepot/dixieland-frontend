0672af98b6e68177685e4258d2edd994ee578000
Dixieland-Money <96084322+DDepot@users.noreply.github.com>
2021-12-15 08:12:57 -0700

    Update index.tsx

--- src/views/Bond/Zapin/ChooseToken/index.tsx
+++ src/views/Bond/Zapin/ChooseToken/index.tsx
@@ -9,8 +9,8 @@ import { Skeleton } from "@material-ui/lab";
 import useTokens, { IAllTokenData } from "../../../../hooks/tokens";
 import { trim } from "../../../../helpers";
 import { IAllBondData } from "../../../../hooks/bonds";
-import { mim, wavax } from "../../../../helpers/bond";
-import { mim as mimToken, wavax as wavaxToken } from "../../../../helpers/tokens";
+import { guac, wsgb } from "../../../../helpers/bond";
+import { guac as mimToken, wsgb as wsgbToken } from "../../../../helpers/tokens";
 
 interface IChooseTokenProps {
     open: boolean;
@@ -24,7 +24,7 @@ function ChooseToken({ open, handleClose, handleSelect, bond }: IChooseTokenProp
 
     const [quantity, setQuantity] = useState("");
 
-    const filtredTokens = tokens.filter(({ name, address, isAvax }) => {
+    const filtredTokens = tokens.filter(({ name, address, isSgb }) => {
         let addressTest = true;
 
         if (quantity && quantity.length === 42) {
@@ -39,12 +39,12 @@ function ChooseToken({ open, handleClose, handleSelect, bond }: IChooseTokenProp
 
         let lpFilter = true;
 
-        if (bond.name === mim.name) {
-            lpFilter = mimToken.address !== address;
+        if (bond.name === guac.name) {
+            lpFilter = guacToken.address !== address;
         }
 
-        if (bond.name === wavax.name) {
-            lpFilter = isAvax ? false : wavaxToken.address !== address;
+        if (bond.name === wsgb.name) {
+            lpFilter = isSgb ? false : wsgbToken.address !== address;
         }
 
         return nameTest && addressTest && lpFilter;
