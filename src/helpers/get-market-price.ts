be6fc132cabe2373dbd87a7a411583c98088a1cc
Dixieland-Money <96084322+DDepot@users.noreply.github.com>
2021-12-16 17:12:27 -0700

    Update get-market-price.ts

--- src/helpers/get-market-price.ts
+++ src/helpers/get-market-price.ts
@@ -4,7 +4,7 @@ import { guacJazz } from "../helpers/bond";
 import { Networks } from "../constants/blockchain";
 
 export async function getMarketPrice(networkID: Networks, provider: ethers.Signer | ethers.providers.Provider): Promise<number> {
-    const guacJazz Address = guacJazz.getAddressForReserve(networkID);
+    const guacJazzAddress = guacJazz.getAddressForReserve(networkID);
     const pairContract = new ethers.Contract(guacJazzAddress, LpReserveContract, provider);
     const reserves = await pairContract.getReserves();
     const marketPrice = reserves[0] / reserves[1];
