4f21b04bb2d7f573bfae70532c2b6e65a4de6242
Dixieland-Money <96084322+DDepot@users.noreply.github.com>
2021-12-20 15:40:46 -0700

    Update blockchain.ts
    check chain id 19

--- src/constants/blockchain.ts
+++ src/constants/blockchain.ts
@@ -1,7 +1,7 @@
 export const TOKEN_DECIMALS = 18;
 
 export enum Networks {
-    SGB = 19,                       //not sure if chian id is correct
+    SGB = 19,                      
 }
 
 export const DEFAULT_NETWORK = Networks.SGB;
