1e3969ae0dee7e595f5ad5cabf7aa9653db1ea5f
Dixieland-Money <96084322+DDepot@users.noreply.github.com>
2021-12-20 22:44:46 -0700

    Update token-price.ts

--- src/helpers/token-price.ts
+++ src/helpers/token-price.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const cache: { [key: string]: number } = {};
 
 export const loadTokenPrices = async () => {
-    const url = "https://api.coingecko.com/api/v3/simple/price?ids=songbird,olympus,avocadoge&vs_currencies=usd";
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=songbird,dogecoin&vs_currencies=usd";
     const { data } = await axios.get(url);
 
     cache["SGB"] = data["songbird"].usd;
-    cache["GUAC"] = data["avocadoge"].usd;
+    cache["GUAC"] = data["dogecoin"].usd;
 };
 
 export const getTokenPrice = (symbol: string): number => {
